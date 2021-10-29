9b06d7a59228cb2c43c28f4c3ce2b206f04458a4
Adam Wolkowycki <adwol21@student.sdu.dk>
2021-10-30 00:23:45 +0200

    Initial Commit

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by USER on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
